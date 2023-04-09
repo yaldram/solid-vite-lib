9e52aedeaa45a755e7c3b2c394b00dbd80e24a41
Arsalan <arsalanyaldram0211@outlook.com>
2023-04-09 14:53:30 +0530

    feat: setup vite library, added storybook & ran build successfully.

---
+++ src/components/atoms/layouts/box/box.stories.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource solid-js */
+
+import { Box } from ".";
+
+export default {
+  title: "Atoms/Layout/Box",
+};
+
+export const Default = () => (
+  <Box style="padding: 1rem; background: green; color: white; width: 100%">
+    Box Component
+  </Box>
+);
---
+++ src/components/atoms/layouts/box/index.tsx
@@ -0,0 +1,7 @@
+import { Component, ComponentProps } from "solid-js";
+
+export type BoxProps = ComponentProps<"div">;
+
+export const Box: Component<BoxProps> = (props) => {
+  return <div {...props} />;
+};
